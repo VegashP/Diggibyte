9445aa8946c42582a280b49abfcfdf10c5887954
VegashP <vsh.vegashp@gmail.com>
2025-06-05 11:37:10 +0530

    Basics of Python with Leetcode Questions

---
+++ Python/Basics.py
@@ -0,0 +1,39 @@
+# Printing Statement with operators
+
+print(45+5)
+print(50-5)
+print(10*5)
+print(500/5)
+print(500%2)
+print(55//2)
+print(5**5)
+
+
+# Order of Execution in Operators
+#Paraentheses -> Exponent -> Multi -> Div -> Add -> Add -> Sub
+print(89-43+39/65*3**2+(4+5))
+
+#Naming of Variable
+'''
+1. Use can Letter, Digit and Underscore
+2. Every Variable name must begin with letter
+3. Variable name can't be Keyword like input, print
+'''
+
+
+'''
+Example
+Create four Varible for Travelling
+Cost = 4500
+Insurance = 'Paid'
+Passport = 'India'
+Baggage = 2
+'''
+
+
+#Entering User Data in Python
+country = input("Enter Your Country Name: ")  #   <- String
+print("Country Name is: ",country)
+
+Cost = int(input("Enter You total Cost: ")) # TypeCasting to int
+print("The Total Cost is:  ",Cost)
---
+++ Python/Leetcode/Leetcode_27.py
@@ -0,0 +1,23 @@
+'''
+Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+'''
+from unittest import removeResult
+def removeElement(nums, val):
+    """
+    :type nums: List[int]
+    :type val: int
+    :rtype: int
+    """
+    nums[:] = [x for x in nums if x!=val]
+    return len(nums)
+
+
+nums = [0,1,2,2,3,0,4,2]
+val = 2
+print(removeElement(nums,val))
+
